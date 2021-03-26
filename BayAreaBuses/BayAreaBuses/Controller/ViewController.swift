adf6df2c7a0fb25dfeb005444ecc105d6d313988
Mondale Felix <mondalefelix@gmail.com>
2021-03-25 23:26:01 -0700

    Displays users location with Core Location

--- BayAreaBuses/BayAreaBuses/Controller/ViewController.swift
+++ BayAreaBuses/BayAreaBuses/Controller/ViewController.swift
@@ -13,26 +13,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var locationManager = CLLocationManager()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationManager.delegate = self
-        // 2
-        locationManager.requestAlwaysAuthorization()
-        // 3
-        
+        setUpLocation()
         setUpMap()
     }
     
     private func setUpMap(){
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let latitide = locationManager.location?.coordinate.latitude ?? 122.0090
+        let longitude = locationManager.location?.coordinate.longitude ?? 37.3330
+        
+        let camera = GMSCameraPosition.camera(withLatitude: latitide, longitude: longitude, zoom: 15.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         self.view.addSubview(mapView)
 
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-
+        marker.position = CLLocationCoordinate2D(latitude: latitide, longitude: longitude)
         marker.map = mapView
+    }
+    
+    private func setUpLocation(){
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
 }
