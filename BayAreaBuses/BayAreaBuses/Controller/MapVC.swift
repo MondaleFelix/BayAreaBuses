bfb4a28dfa4d5a16ac7404a04ed5ec3cecc40c8d
Mondale Felix <mondalefelix@gmail.com>
2021-03-27 02:20:31 -0700

    Configured Keyboard and presents routes VC

--- BayAreaBuses/BayAreaBuses/Controller/MapVC.swift
+++ BayAreaBuses/BayAreaBuses/Controller/MapVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import GoogleMaps
 import CoreLocation
 
-class MapVC: UIViewController, CLLocationManagerDelegate {
+class MapVC: UIViewController, CLLocationManagerDelegate{
     
     var locationManager = CLLocationManager()
     let searchBar = BABTextField()
@@ -19,20 +19,32 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
         setUpLocation()
         setUpMap()
         configureSearchBar()
+        createKeyboardTapGesture()
+        
     }
     
+
     private func setUpMap(){
         let latitide = locationManager.location?.coordinate.latitude ?? 122.0090
         let longitude = locationManager.location?.coordinate.longitude ?? 37.3330
         
         let camera = GMSCameraPosition.camera(withLatitude: latitide, longitude: longitude, zoom: 15.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        mapView.delegate = self
         self.view.addSubview(mapView)
 
+        
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: latitide, longitude: longitude)
         marker.map = mapView
+
+
+    }
+    
+    
+    func createKeyboardTapGesture(){
+
     }
     
     private func setUpLocation(){
@@ -42,6 +54,7 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
     
     private func configureSearchBar(){
         view.addSubview(searchBar)
+        searchBar.delegate = self
 
         NSLayoutConstraint.activate([
             searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
@@ -50,5 +63,20 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
             searchBar.heightAnchor.constraint(equalToConstant: 35)
         ])
         
+    }
+}
+
+extension MapVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let routesVC = RoutesVC()
+        routesVC.modalPresentationStyle = .popover
+        self.present(routesVC, animated: true, completion: nil)
+        return true
+    }
+}
+
+extension MapVC: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        view.endEditing(true)
     }
 }
